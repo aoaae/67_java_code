0b65424418e588f19ba0c6d765a69316fdf3ded5
aoaae <60562492+aoaae@users.noreply.github.com>
2024-09-29 22:57:51 +0700

    for week 14

--- W14_00_SumNumber.java
+++ W14_00_SumNumber.java
@@ -8,7 +8,6 @@
 
 
 package Temp;
-import java.util.Scanner;
 
 public class W14_00_SumNumber {
     public static void main(String[] args) {
@@ -17,41 +16,29 @@ public class W14_00_SumNumber {
 
         // 1. แสดงรายการข้อมูลใน Array ทั้งหมด
         System.out.println("Data in Array are:");
-        for (int i = 0; i < numbers.length; i++){ 
-            System.out.print(numbers[i]+ " ");
-        }
+  
+
         System.out.println("\n====================================");
+
+
         
         // 2. หาผลรวมของข้อมูลทั้งหมด
-        System.out.println("Data in Array are:");
-        int sum=0;
-        for (int i = 0; i < numbers.length; i++){ 
-            System.out.print(numbers[i]+ " ");
-            sum = sum + numbers[i];   // sum += numbers[i]
-        }
-        System.out.println("\nSum of Number = "+sum);
+
         System.out.println("====================================");
 
+
+
         // 3. สร้าง Array ใหม่เพื่อรับค่าข้อมูลตัวเลข ผ่านคีย์บอร์ด 5 ตัว
-        int [] newNumbers = new int[5];
-        Scanner kb = new Scanner(System.in);
-        for (int i = 0; i < newNumbers.length; i++) {
-            System.out.print("Enter number "+(i+1)+": ");
-            newNumbers[i] = kb.nextInt();
-        }
+                
+        System.out.println("====================================");
+
 
         // 4. แสดงข้อมูลทั้งหมดใน Array ใหม่
         // 5. หาผลรวมของข้อมูลทั้งหมดใน Array ใหม่
-        System.out.println("Data in New Array are:");
-        sum=0;
-        for (int i = 0; i < newNumbers.length; i++){ 
-            System.out.print(newNumbers[i]+ " ");
-            sum = sum + newNumbers[i];   // sum += newNumbers[i]
-        }
-        System.out.println("\nSum of Number = "+sum);
+
+
         System.out.println("====================================");
 
-        kb.close();
 
     }
 }
