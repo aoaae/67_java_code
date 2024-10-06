62c501da5829e91dad7f11c3b26ad471a0347292
aoaae <60562492+aoaae@users.noreply.github.com>
2024-10-06 14:38:50 +0700

    Week 15 for Student

---
+++ W15_02_method_calculator.java
@@ -0,0 +1,32 @@
+package Temp;
+
+public class W15_02_method_calculator {
+    // ทั้งหมดเป็น method ที่มีการรับค่าและคืนค่า
+    // 1) Method สำหรับบวกเลข
+
+
+    // 2) Method สำหรับลบเลข
+
+
+    // 3) Method สำหรับคูณเลข
+
+
+    // 4) Method สำหรับหารเลข
+
+
+    public static void main(String[] args) {
+        // 1) Method สำหรับบวกเลข
+        
+
+        // 2) Method สำหรับลบเลข
+        
+
+        // 3) Method สำหรับคูณเลข
+        
+
+        // 4) Method สำหรับหารเลข
+        
+    }
+
+
+}
---
+++ W15_03_method_calculator_kb.java
@@ -0,0 +1,37 @@
+package Temp;
+
+import java.util.Scanner;
+
+public class W15_03_method_calculator_kb {
+    // ทั้งหมดเป็น method ที่มีการรับค่าและคืนค่า
+    // 1) Method สำหรับบวกเลข
+
+    // 2) Method สำหรับลบเลข
+
+
+    // 3) Method สำหรับคูณเลข
+
+
+    // 4) Method สำหรับหารเลข
+
+
+    public static void main(String[] args) {
+
+        //  รับค่าจากคีย์บอร์ดก่อนส่งไปยัง method อื่น
+
+
+        // 1) Method สำหรับบวกเลข
+        
+
+        // 2) Method สำหรับลบเลข
+        
+
+        // 3) Method สำหรับคูณเลข
+        
+
+        // 4) Method สำหรับหารเลข
+        
+        
+        
+    }
+}
